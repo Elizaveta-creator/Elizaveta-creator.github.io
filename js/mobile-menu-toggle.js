4ec679914f97327fed4f6bcc7866d731ac41de2b
Elizaveta-creator <62671712+Elizaveta-creator@users.noreply.github.com>
2020-09-30 12:37:01 +0300

    Add files via upload

--- js/mobile-menu-toggle.js
+++ js/mobile-menu-toggle.js
@@ -1,6 +1,33 @@
-let menuElem = document.getElementById('menu');
-    let titleElem = menuElem.querySelector('.menu-mobile__title');
+$(document).ready(function() {
+	var triggerOpen		= $('#button-group__input');
+	var triggerClose 	= $('#dropdown-menu').find('li');
+	var marka 			= $('#button-group__icon');
 
-    titleElem.onclick = function() {
-      menuElem.classList.toggle('open');
-    };
+	// set initial Marka icon
+	var m = new Marka('#button-group__icon');
+	m.set('triangle').size(10);
+	m.rotate('down');
+
+	// trigger dropdown
+    triggerOpen.add(marka).on('click', function(e) {
+		e.preventDefault();
+		$('#dropdown-menu').add(triggerOpen).toggleClass('open');
+
+
+		if($('#button-group__icon').hasClass("marka-icon-times")) {
+			m.set('triangle').size(10);
+		} else {
+			m.set('times').size(15);
+		}
+	});
+
+	triggerClose.on('click', function() {
+		// set new placeholder for demo
+		var options = $(this).find('a').html();
+		triggerOpen.text(options);
+
+		$('#dropdown-menu').add(triggerOpen).toggleClass('open');
+		m.set('triangle').size(10);
+	});
+
+});
